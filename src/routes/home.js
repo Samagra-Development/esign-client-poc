c770f83c574857ddec32ebdec6c30bb669fe10ab
amit-s19 <coutamit.s19@gmail.com>
2023-08-17 15:33:42 +0530

    Updated Frontend

--- src/routes/home.js
+++ src/routes/home.js
@@ -10,6 +10,9 @@ function Home() {
     const refNumber = useRef('');
     const [modal, showModal] = useState(false);
     const [response, setResponse] = useState({});
+    const [name, setName] = useState('');
+    const [file, setFile] = useState(null);
+    const [signedFile, setSignedFile] = useState(null);
 
     useEffect(() => {
         let refNumberNew = uuidv4()
@@ -38,6 +41,9 @@ function Home() {
             if (dbRes?.data?.data?.esign_poc?.length) {
                 setResponse(dbRes?.data?.data?.esign_poc?.[0]);
                 showModal(false);
+                let fileBlob = converBase64toBlob(dbRes?.data?.data?.esign_poc?.[0].signed_data, 'application/pdf');
+                var blobURL = URL.createObjectURL(fileBlob);
+                setSignedFile(blobURL);
                 clearInterval(pollInterval);
             }
         }, 5000)
@@ -45,39 +51,72 @@ function Home() {
     })
 
 
+    const handleFileUpload = (e) => {
+        console.log(e.target.files[0])
+        var reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]);
+        reader.onload = function () {
+            setFile(reader.result.slice(28))
+        };
+        reader.onerror = function (error) {
+            alert('Unable to read file!')
+            setName('')
+            setFile(null);
+        };
+    }
+
+    function converBase64toBlob(content, contentType) {
+        contentType = contentType || '';
+        var sliceSize = 512;
+        var byteCharacters = window.atob(content); //method which converts base64 to binary
+        var byteArrays = [
+        ];
+        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            var slice = byteCharacters.slice(offset, offset + sliceSize);
+            var byteNumbers = new Array(slice.length);
+            for (var i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+            var byteArray = new Uint8Array(byteNumbers);
+            byteArrays.push(byteArray);
+        }
+        var blob = new Blob(byteArrays, {
+            type: contentType
+        }); //statement which creates the blob
+        return blob;
+    }
+
     return (
         <div className="App">
             <div className='title'>E-Sign POC</div>
-            {Object.keys(response)?.length == 0 && <form action={`${process.env.REACT_APP_ESIGN_GATEWAY_URL}`} target='_blank' id="frmdata" method="post">
-                <input id="Name" name="Name" type="hidden" value="Test Esigner" />
-                <input id="FileType" name="FileType" type="hidden" value="PDF" />
-                <input id="File" name="File" type="hidden" value="JVBERi0xLjMNCiXi48/TDQoNCjEgMCBvYmoNCjw8DQovVHlwZSAvQ2F0YWxvZw0KL091dGxpbmVzIDIgMCBSDQovUGFnZXMgMyAwIFINCj4+DQplbmRvYmoNCg0KMiAwIG9iag0KPDwNCi9UeXBlIC9PdXRsaW5lcw0KL0NvdW50IDANCj4+DQplbmRvYmoNCg0KMyAwIG9iag0KPDwNCi9UeXBlIC9QYWdlcw0KL0NvdW50IDINCi9LaWRzIFsgNCAwIFIgNiAwIFIgXSANCj4+DQplbmRvYmoNCg0KNCAwIG9iag0KPDwNCi9UeXBlIC9QYWdlDQovUGFyZW50IDMgMCBSDQovUmVzb3VyY2VzIDw8DQovRm9udCA8PA0KL0YxIDkgMCBSIA0KPj4NCi9Qcm9jU2V0IDggMCBSDQo+Pg0KL01lZGlhQm94IFswIDAgNjEyLjAwMDAgNzkyLjAwMDBdDQovQ29udGVudHMgNSAwIFINCj4+DQplbmRvYmoNCg0KNSAwIG9iag0KPDwgL0xlbmd0aCAxMDc0ID4+DQpzdHJlYW0NCjIgSg0KQlQNCjAgMCAwIHJnDQovRjEgMDAyNyBUZg0KNTcuMzc1MCA3MjIuMjgwMCBUZA0KKCBBIFNpbXBsZSBQREYgRmlsZSApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDY4OC42MDgwIFRkDQooIFRoaXMgaXMgYSBzbWFsbCBkZW1vbnN0cmF0aW9uIC5wZGYgZmlsZSAtICkgVGoNCkVUDQpCVA0KL0YxIDAwMTAgVGYNCjY5LjI1MDAgNjY0LjcwNDAgVGQNCigganVzdCBmb3IgdXNlIGluIHRoZSBWaXJ0dWFsIE1lY2hhbmljcyB0dXRvcmlhbHMuIE1vcmUgdGV4dC4gQW5kIG1vcmUgKSBUag0KRVQNCkJUDQovRjEgMDAxMCBUZg0KNjkuMjUwMCA2NTIuNzUyMCBUZA0KKCB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDYyOC44NDgwIFRkDQooIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlICkgVGoNCkVUDQpCVA0KL0YxIDAwMTAgVGYNCjY5LjI1MDAgNjE2Ljg5NjAgVGQNCiggdGV4dC4gQW5kIG1vcmUgdGV4dC4gQm9yaW5nLCB6enp6ei4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kICkgVGoNCkVUDQpCVA0KL0YxIDAwMTAgVGYNCjY5LjI1MDAgNjA0Ljk0NDAgVGQNCiggbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDU5Mi45OTIwIFRkDQooIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlIHRleHQuICkgVGoNCkVUDQpCVA0KL0YxIDAwMTAgVGYNCjY5LjI1MDAgNTY5LjA4ODAgVGQNCiggQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgKSBUag0KRVQNCkJUDQovRjEgMDAxMCBUZg0KNjkuMjUwMCA1NTcuMTM2MCBUZA0KKCB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBFdmVuIG1vcmUuIENvbnRpbnVlZCBvbiBwYWdlIDIgLi4uKSBUag0KRVQNCmVuZHN0cmVhbQ0KZW5kb2JqDQoNCjYgMCBvYmoNCjw8DQovVHlwZSAvUGFnZQ0KL1BhcmVudCAzIDAgUg0KL1Jlc291cmNlcyA8PA0KL0ZvbnQgPDwNCi9GMSA5IDAgUiANCj4+DQovUHJvY1NldCA4IDAgUg0KPj4NCi9NZWRpYUJveCBbMCAwIDYxMi4wMDAwIDc5Mi4wMDAwXQ0KL0NvbnRlbnRzIDcgMCBSDQo+Pg0KZW5kb2JqDQoNCjcgMCBvYmoNCjw8IC9MZW5ndGggNjc2ID4+DQpzdHJlYW0NCjIgSg0KQlQNCjAgMCAwIHJnDQovRjEgMDAyNyBUZg0KNTcuMzc1MCA3MjIuMjgwMCBUZA0KKCBTaW1wbGUgUERGIEZpbGUgMiApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDY4OC42MDgwIFRkDQooIC4uLmNvbnRpbnVlZCBmcm9tIHBhZ2UgMS4gWWV0IG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gKSBUag0KRVQNCkJUDQovRjEgMDAxMCBUZg0KNjkuMjUwMCA2NzYuNjU2MCBUZA0KKCBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDY2NC43MDQwIFRkDQooIHRleHQuIE9oLCBob3cgYm9yaW5nIHR5cGluZyB0aGlzIHN0dWZmLiBCdXQgbm90IGFzIGJvcmluZyBhcyB3YXRjaGluZyApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDY1Mi43NTIwIFRkDQooIHBhaW50IGRyeS4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gKSBUag0KRVQNCkJUDQovRjEgMDAxMCBUZg0KNjkuMjUwMCA2NDAuODAwMCBUZA0KKCBCb3JpbmcuICBNb3JlLCBhIGxpdHRsZSBtb3JlIHRleHQuIFRoZSBlbmQsIGFuZCBqdXN0IGFzIHdlbGwuICkgVGoNCkVUDQplbmRzdHJlYW0NCmVuZG9iag0KDQo4IDAgb2JqDQpbL1BERiAvVGV4dF0NCmVuZG9iag0KDQo5IDAgb2JqDQo8PA0KL1R5cGUgL0ZvbnQNCi9TdWJ0eXBlIC9UeXBlMQ0KL05hbWUgL0YxDQovQmFzZUZvbnQgL0hlbHZldGljYQ0KL0VuY29kaW5nIC9XaW5BbnNpRW5jb2RpbmcNCj4+DQplbmRvYmoNCg0KMTAgMCBvYmoNCjw8DQovQ3JlYXRvciAoUmF2ZSBcKGh0dHA6Ly93d3cubmV2cm9uYS5jb20vcmF2ZVwpKQ0KL1Byb2R1Y2VyIChOZXZyb25hIERlc2lnbnMpDQovQ3JlYXRpb25EYXRlIChEOjIwMDYwMzAxMDcyODI2KQ0KPj4NCmVuZG9iag0KDQp4cmVmDQowIDExDQowMDAwMDAwMDAwIDY1NTM1IGYNCjAwMDAwMDAwMTkgMDAwMDAgbg0KMDAwMDAwMDA5MyAwMDAwMCBuDQowMDAwMDAwMTQ3IDAwMDAwIG4NCjAwMDAwMDAyMjIgMDAwMDAgbg0KMDAwMDAwMDM5MCAwMDAwMCBuDQowMDAwMDAxNTIyIDAwMDAwIG4NCjAwMDAwMDE2OTAgMDAwMDAgbg0KMDAwMDAwMjQyMyAwMDAwMCBuDQowMDAwMDAyNDU2IDAwMDAwIG4NCjAwMDAwMDI1NzQgMDAwMDAgbg0KDQp0cmFpbGVyDQo8PA0KL1NpemUgMTENCi9Sb290IDEgMCBSDQovSW5mbyAxMCAwIFINCj4+DQoNCnN0YXJ0eHJlZg0KMjcxNA0KJSVFT0YNCg==" />
-                <input id="ReferenceNumber" name="ReferenceNumber" type="hidden" value={refNumber.current} />
-                <input id="SelectPage" name="SelectPage" type="hidden" value="FIRST" />
-                <input id="SignatureType" name="SignatureType" type="hidden" value="0" />
-                <input id="SignaturePosition" name="SignaturePosition" type="hidden" value="Bottom-Right" />
-                <input id="SignatureMode" name="SignatureMode" type="hidden" value="12" />
-                <input id="AuthToken" name="AuthToken" type="hidden" value={`${process.env.REACT_APP_ESIGN_AUTH_TOKEN}`} />
-                <input id="AuthenticationMode" name="AuthenticationMode" type="hidden" value="1" />
-                <input id="PageNumber" name="PageNumber" type="hidden" value="" />
-                <input id="SUrl" name="SUrl" type="hidden" value={`${process.env.REACT_APP_ESIGN_CALLBACK_URL}/success`} />
-                <input id="FUrl" name="FUrl" type="hidden" value={`${process.env.REACT_APP_ESIGN_CALLBACK_URL}/failure`} />
-                <input id="CUrl" name="CUrl" type="hidden" value={`${process.env.REACT_APP_ESIGN_CALLBACK_URL}/failure`} />
-                <input id="eSign_SignerID" name="eSign_SignerID" type="hidden" value="" />
-                <button class="btn btn-primary" type="submit" name="formAction2" id="btnEsignKYC" value="EsignWithASP">Start Signing</button>
-            </form>}
+            {Object.keys(response)?.length == 0 && <div className='formInput'>
+                <div>Name <input type={"text"} placeholder="Your Name" onChange={e => setName(e.target.value)} value={name} /></div>
+                <div>Document <input type={"file"} accept="application/pdf" onChange={handleFileUpload} /></div>
+                {name && file && <form action={`${process.env.REACT_APP_ESIGN_GATEWAY_URL}`} target='_blank' id="frmdata" method="post" >
+                    <input id="Name" name="Name" type="hidden" value={name} />
+                    <input id="FileType" name="FileType" type="hidden" value="PDF" />
+                    <input id="File" name="File" type="hidden" value={file} />
+                    <input id="ReferenceNumber" name="ReferenceNumber" type="hidden" value={refNumber.current} />
+                    <input id="SelectPage" name="SelectPage" type="hidden" value="FIRST" />
+                    <input id="SignatureType" name="SignatureType" type="hidden" value="0" />
+                    <input id="SignaturePosition" name="SignaturePosition" type="hidden" value="Bottom-Right" />
+                    <input id="SignatureMode" name="SignatureMode" type="hidden" value="12" />
+                    <input id="AuthToken" name="AuthToken" type="hidden" value={`${process.env.REACT_APP_ESIGN_AUTH_TOKEN}`} />
+                    <input id="AuthenticationMode" name="AuthenticationMode" type="hidden" value="1" />
+                    <input id="PageNumber" name="PageNumber" type="hidden" value="" />
+                    <input id="SUrl" name="SUrl" type="hidden" value={`${process.env.REACT_APP_ESIGN_CALLBACK_URL}/success`} />
+                    <input id="FUrl" name="FUrl" type="hidden" value={`${process.env.REACT_APP_ESIGN_CALLBACK_URL}/failure`} />
+                    <input id="CUrl" name="CUrl" type="hidden" value={`${process.env.REACT_APP_ESIGN_CALLBACK_URL}/failure`} />
+                    <input id="eSign_SignerID" name="eSign_SignerID" type="hidden" value="" />
+                    <button class="btn btn-primary" type="submit" name="formAction2" id="btnEsignKYC" value="EsignWithASP">Start Signing</button>
+                </form>}
+            </div>}
             {Object.keys(response)?.length > 0 && response.status == "Success" &&
                 <>
-                    <div className='esign-text'>Your E-Signed Document</div>
+                    <div className='esign-text'>Your document is signed successfully ✅ </div>
                     <div style={{ color: '#fff', marginTop: 10 }}>Transaction ID: {response.transaction_number}</div>
                     <div style={{ color: '#fff', marginTop: 5 }}>Reference No: {response.ref_number}</div>
-                    <div className='pdf-container'>
-                        <PDFViewer
-                            document={{
-                                base64: response.signed_data,
-                            }}
-                        />
-                    </div>
+                    <div style={{ padding: "0.5rem 2rem", borderRadius: 5, cursor: 'pointer', background: '#ffc119', color: '#000', marginTop: 30 }} onClick={() => window.open(signedFile)}>Download Signed Document</div>
                 </>
             }
             {Object.keys(response)?.length > 0 && response.status == "Failure" &&
@@ -99,3 +138,4 @@ function Home() {
 }
 
 export default Home;
+
